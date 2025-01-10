4c0f55f5777d78026501e42ede540a011aca0868
FirdausyTama <atama.firdausy@gmail.com>
2025-01-10 22:16:19 +0700

    membuat data class MahasiswaEvent

--- app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.firebase.ui.home.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.firebase.repository.RepositoryMhs
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs
+) : ViewModel() {
+
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkat: String = "",
+)
