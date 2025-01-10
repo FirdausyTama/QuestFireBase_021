3bc6194e38d3dedd8cb9aca9dc3fc340a5c51e7f
FirdausyTama <atama.firdausy@gmail.com>
2025-01-10 22:42:02 +0700

    membuat validasi fields input

--- app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/home/viewmodel/InsertViewModel.kt
@@ -1,13 +1,42 @@
 package com.example.firebase.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.firebase.repository.RepositoryMhs
+import java.text.Normalizer.Form
 
 class InsertViewModel (
     private val mhs: RepositoryMhs
 ) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
 
+    //memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+    //Validasi Data input pengguna
+    fun validateFields(): Boolean{
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
 }
 
 sealed class FormState {
