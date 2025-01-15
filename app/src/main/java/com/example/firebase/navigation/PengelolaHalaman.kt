2bbc88d3ff55765a4d3024726662990ae4e0c416
FirdausyTama <atama.firdausy@gmail.com>
2025-01-15 17:00:30 +0700

    membuat rute navigasi

--- app/src/main/java/com/example/firebase/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebase/navigation/PengelolaHalaman.kt
@@ -3,37 +3,59 @@ package com.example.firebase.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.firebase.ui.home.pages.DetailScreen
 import com.example.firebase.ui.home.pages.HomeScreen
 import com.example.firebase.ui.home.pages.InsertMhsView
 
 
 @Composable
 fun PengelolaHalaman(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
-){
+) {
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
-        modifier = Modifier
-    ){
-        composable(DestinasiHome.route){
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = {
                     navController.navigate(DestinasiInsert.route)
                 },
+                onDetailClick = { nim ->
+                    navController.navigate(DestinasiDetail.createRoute(nim))
+                }
             )
         }
 
-        composable(DestinasiInsert.route){
+        composable(DestinasiInsert.route) {
             InsertMhsView(
-                onBack = {navController.popBackStack()},
+                onBack = { navController.popBackStack() },
                 onNavigate = {
-                    navController.navigate(DestinasiHome.route)
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
+                    }
                 }
+            )
+        }
+
+        composable(
+            route = DestinasiDetail.route,
+            arguments = listOf(
+                navArgument("nim") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: ""
+            DetailScreen(
+                docId = nim,
+                navigateBack = { navController.popBackStack() },
+                navigateToEdit = {}
             )
         }
     }
