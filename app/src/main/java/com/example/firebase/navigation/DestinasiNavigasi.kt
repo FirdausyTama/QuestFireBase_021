c2dcf6bb6666f0c7acf62e042062b7fff20d079d
FirdausyTama <atama.firdausy@gmail.com>
2025-01-15 17:00:46 +0700

    membuat destinasi navigasi halaman

--- app/src/main/java/com/example/firebase/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/firebase/navigation/DestinasiNavigasi.kt
@@ -14,3 +14,9 @@ object DestinasiInsert : DestinasiNavigasi {
     override val route: String = "insert"
     override val titleRes: String = "Insert"
 }
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route: String = "detail/{nim}"
+    override val titleRes: String = "Detail"
+    fun createRoute(nim: String): String = "detail/$nim"
+}
